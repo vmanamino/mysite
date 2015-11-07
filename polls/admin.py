739fc9129b65cf9a67cabc4d4945ebcf1c4136d0
Viral Amin <vmanamino@gmail.com>
2015-11-07 20:35:53 +0000

    config-ed

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,9 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
   fieldsets = [ (None, {'fields': ['question_text']}), ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
   inlines = [ChoiceInline]
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
