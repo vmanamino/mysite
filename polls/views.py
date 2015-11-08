1b9bd16d006c5d4ed50963b5291175982861b451
Viral Amin <vmanamino@gmail.com>
2015-11-08 22:02:02 +0000

    generic views

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
