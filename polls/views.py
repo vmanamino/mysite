f045c1ada306bfd0ca5c373f80b7fd5da80a2935
Viral Amin <vmanamino@gmail.com>
2015-11-09 04:06:14 +0000

    basic #index, #detail tests

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -10,27 +11,21 @@ class IndexView(generic.ListView):
   context_object_name = 'latest_question_list'
 
   def get_queryset(self):
-    """Return the last five published questions."""
-    return Question.objects.order_by('-pub_date')[:5]
+    """Return the last five published questions excluding those to be published in the future."""
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-# def index(request):
-#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#   template = loader.get_template('polls/index.html')
-#   context = {'latest_question_list': latest_question_list}
-#   return render(request, 'polls/index.html', context)
 class DetailView(generic.DetailView):
   model = Question
   template_name = 'polls/detail.html'
+  def get_queryset(self):
+    """
+    Excludes questions unpublished
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now())
 
-# def detail(request, question_id):
-#   question = get_object_or_404(Question, pk=question_id)
-#   return render(request, 'polls/detail.html', {'question': question})
 class ResultsView(generic.DetailView):
   model = Question
   template_name = 'polls/results.html'
-# def results(request, question_id):
-#   question = get_object_or_404(Question, pk=question_id)
-#   return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
   p = get_object_or_404(Question, pk=question_id)
